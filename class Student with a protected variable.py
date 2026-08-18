37b00fcf4aefef7baa69399b388efb66b8509dd6
Your Name <yourmail@example.com>
2026-08-18 23:43:08 +0530

    managing account

---
+++ class Student with a protected variable.py
@@ -0,0 +1,6 @@
+#Create a class Student with a protected variable _marks = 80. Create a method show() to display the marks.
+class student:
+    def __init__(self):
+        self._marks = 80
+    def show()
+        print ()
